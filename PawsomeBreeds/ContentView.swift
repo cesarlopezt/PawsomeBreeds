4c4faf9fdbcd675f44b9017c579edb0c1f3c00ae
cesarlopezt <4370350+cesarlopezt@users.noreply.github.com>
2023-06-05 15:27:28 -0500

    Basic fetch, decoding to breeds

--- PawsomeBreeds/ContentView.swift
+++ PawsomeBreeds/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Cell: View {
+    var breed: Breed
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if let subbreed = breed.subbreed {
+            Text("\(subbreed) \(breed.name)")
+        } else {
+            Text("\(breed.name)")
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var response: BreedListResponse? = nil
+    var body: some View {
+        List {
+            if let response {
+                ForEach(response.message.breeds) { breed in
+                    Cell(breed: breed)
+                }
+            }
+        }
+        .task {
+            do {
+                let url = URL(string:"https://dog.ceo/api/breeds/list/all")!
+                var request = URLRequest(url: url)
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.httpMethod = "GET"
+                
+                let (data, _) = try await URLSession.shared.data(for: request)
+                let res = try JSONDecoder().decode(BreedListResponse.self, from: data)
+                print(res)
+                response = res
+            } catch {
+                print(error)
+            }
+            
+        }
     }
 }
 
---
+++ PawsomeBreeds/Networking/Responses.swift
@@ -0,0 +1,13 @@
+//
+//  Responses.swift
+//  PawsomeBreeds
+//
+//  Created by Cesar Lopez on 6/5/23.
+//
+
+import Foundation
+
+struct BreedListResponse: Codable {
+    let message: BreedList
+    let status: String
+}
