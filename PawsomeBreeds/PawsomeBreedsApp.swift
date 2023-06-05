4c1ff4aff8e7dbb592782104813a9d4adf496a29
cesarlopezt <4370350+cesarlopezt@users.noreply.github.com>
2023-06-05 12:32:04 -0500

    Initial Commit

---
+++ PawsomeBreeds/PawsomeBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsomeBreedsApp.swift
+//  PawsomeBreeds
+//
+//  Created by Cesar Lopez on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PawsomeBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
